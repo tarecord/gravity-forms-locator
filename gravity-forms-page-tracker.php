4d0735c4fd3f2806ed00e62bcddc6f85d04594f1
Tanner Record <tanner.record@northstarmarketing.com>
2017-01-04 10:53:27 -0500

    Setup Activation/Deactivation hooks

--- gravity-forms-page-tracker.php
+++ gravity-forms-page-tracker.php
@@ -24,3 +24,62 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+/*
+ * Registered Actions, Filters and Hooks
+ */
+ register_activation_hook( __FILE__, 'gravity_forms_page_tracker_activate' );
+ register_deactivation_hook( __FILE__, 'gravity_forms_page_tracker_deactivate' );
+ register_uninstall_hook( __FILE__, 'gravity_forms_page_tracker_uninstall' );
+
+// Update the table with the page/form id when the post is saved
+ add_action( 'save_post', 'update_form_page_id' );
+ 
+/*
+ * Global Variables
+ */
+
+global $gform_form_page_table, $wpdb;
+$gform_form_page_table = $wpdb->prefix . "gform_form_page";
+
+
+
+// Install table when activated
+function gravity_forms_page_tracker_activate () {
+  
+  global $wpdb, $gform_form_page_table;
+ 
+  $charset_collate = $wpdb->get_charset_collate();
+  
+  error_log($charset_collate);
+
+  // Check if table exists before trying to create it
+  if($wpdb->get_var("SHOW TABLES LIKE '$gform_form_page_table'") != $gform_form_page_table){
+    $sql = "CREATE TABLE $gform_form_page_table (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      form_id mediumint(8) NOT NULL,
+      page_id bigint(20) NOT NULL,
+      PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+  }
+  
+}
+
+// Remove table when uninstalled
+function gravity_forms_page_tracker_uninstall () {
+  
+  global $wpdb, $gform_form_page_table;
+  
+  // Check if table exists before trying to delete it
+  if($wpdb->get_var("SHOW TABLES LIKE '$gform_form_page_table'") == $gform_form_page_table){
+    $wpdb->query("DROP TABLE $gform_form_page_table;");
+  }
+  
+}
+
+function update_form_page_id() {
+  
+}
