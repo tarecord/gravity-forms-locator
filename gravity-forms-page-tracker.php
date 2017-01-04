2af76865b8574582cf0de58a6029e5b2162a7b83
Tanner Record <tanner.record@northstarmarketing.com>
2017-01-04 10:06:18 -0500

    Initial commit

---
+++ gravity-forms-page-tracker.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Gravity Forms Page Tracker Addon
+Plugin URI: https://gitlab.com/tanner.record/gravity-forms-page-tracker-addon
+Description: A simple addon that displays what page a form is on.
+Version: 0.1
+Author: Tanner Record
+Author URI: http://www.northstarmarketing.com
+License: GPL2
+*/
+/*
+Copyright 2017  Tanner Record  (email : tanner.record@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
