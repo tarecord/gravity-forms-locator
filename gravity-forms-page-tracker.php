3798f9adcfacb1bd92244ae1759bd939fa6603ae
Tanner Record <tanner.record@northstarmarketing.com>
2017-01-05 22:23:36 -0500

    Add the form/post relation in the table when the post is saved

--- gravity-forms-page-tracker.php
+++ gravity-forms-page-tracker.php
@@ -83,6 +83,61 @@ function gravity_forms_page_tracker_uninstall () {
   
 }
 
-function update_form_page_id() {
+/**
+ * Save page/form relation in the database on save
+ * 
+ * @param  int  $post_id the post id returned from the save_post action
+ * 
+ * @return  null
+ */
+function update_form_page_id($post_id) {
+  
+    // If this is a revision just return
+    if ( wp_is_post_revision( $post_id ) )
+    
+    return;
+    
+    
+    global $wpdb, $gform_form_page_table;
+      
+    // Grab the content from the form post
+    $content = stripslashes($_POST['content']);
+    
+    $pattern = get_shortcode_regex();
+
+    // Check if shortcode exists in the content
+    if (   preg_match_all( '/'. $pattern .'/s', $content, $matches ) && array_key_exists( 2, $matches ) && in_array( 'gravityform', $matches[2] ) ){
+        
+        // Use the match to extract the form id from the shortcode
+        preg_match('~id="(.)\"~', $matches[3][0], $form_id);
+        
+        // Convert the form id to an int
+        $form_id = (int) $form_id[1];
+    }
+    
+    
+    // Add the relationship to the table
+    if($wpdb->get_var("SHOW TABLES LIKE '$gform_form_page_table'") == $gform_form_page_table){
+        
+        // Check to see if the form/post relation already exists in the table
+        $form_post_count = $wpdb->get_var("SELECT COUNT(*) FROM `$gform_form_page_table` WHERE form_id='$form_id' AND post_id='$post_id'");
+        
+        if($form_post_count < 1){
+        
+            $wpdb->insert(
+              	$gform_form_page_table,
+              	array(
+              		'form_id' => $form_id,
+              		'post_id' => $post_id
+              	),
+              	array(
+              		'%d',
+              		'%d'
+              	)
+            );
+          
+        }
+        
+    }
   
 }
