558ed9dee8e2fe212e1f84574b58bd7e7dd6d335
Tanner Record <tanner.record@northstarmarketing.com>
2017-01-09 22:47:23 -0500

    Add the "wrap" div to correctly display table elements

--- includes/location.php
+++ includes/location.php
@@ -1,9 +1,11 @@
+<div class="wrap">
+  
 <h1>Form Locations</h1>
 
-<?php // TODO: Add form post/page association table ?><?php
 <?php
     $locations_table = new Form_Locations_Table();
     $locations_table->prepare_items();
     $locations_table->display();
 ?>
 
+</div>
