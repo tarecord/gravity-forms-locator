f4b291fb1b9453d95351d1f63212fb932d41d3c5
Tanner Record <tanner.record@northstarmarketing.com>
2017-01-17 21:39:50 -0500

    Add simple scan all pages button to let user force re-scan

--- includes/location.php
+++ includes/location.php
@@ -2,6 +2,8 @@
   
 <h1>Form Locations</h1>
 
+<a href="<?php echo wp_nonce_url( admin_url( 'admin.php?page=locations&process=scan_all_pages'), 'process' ); ?>">Scan All Pages</a>
+
 <?php
     $locations_table = new Form_Locations_Table();
     $locations_table->prepare_items();
