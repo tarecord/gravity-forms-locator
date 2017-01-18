b4ec4a4190e77694820e704ac673c6bb896c7c05
Tanner Record <tanner.record@northstarmarketing.com>
2017-01-17 21:38:59 -0500

    Scan the $post->post_content for gravity forms and add the relation

--- classes/WP_Scan_Existing_Forms.php
+++ classes/WP_Scan_Existing_Forms.php
@@ -18,8 +18,32 @@ class WP_Scan_Existing_Forms extends WP_Background_Process {
    *
    * @return mixed
    */
-  protected function task( $item ) {
-    error_log('Process Item: ' . $item);
+  protected function task( $post ) {
+    error_log('Process Item: ' . $post->post_title );
+    // Grab the content from the form post
+    $content = stripslashes($post->post_content);
+    
+    error_log(var_dump($post));
+    
+    $pattern = get_shortcode_regex();
+    
+    // Check if shortcode exists in the content
+    if (   preg_match_all( '/'. $pattern .'/s', $content, $matches ) && array_key_exists( 2, $matches ) && in_array( 'gravityform', $matches[2] ) ){
+        
+        // Use the match to extract the form id from the shortcode
+        preg_match('~id="(.*)\"~', $matches[3][0], $form_id);
+        
+        // Convert the form id to an int
+        $form_id = (int) $form_id[1];
+    }
+    $Gravity_Forms_Page_Tracker = new Gravity_Forms_Page_Tracker();
+    
+    if ( isset($form_id) ){
+      
+      $Gravity_Forms_Page_Tracker->add_form_post_relation($form_id, $post->ID);
+      
+    }
+    
     return false;
   }
   /**
