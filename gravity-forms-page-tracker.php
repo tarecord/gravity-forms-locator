c18e00bd1e6fef51e8063bc23f14c7e29d856b60
Tanner Record <tanner.record@northstarmarketing.com>
2017-01-17 21:42:26 -0500

    Check the process handler on 'init'
    Checks to see if _nonce and process are passed in $_GET

--- gravity-forms-page-tracker.php
+++ gravity-forms-page-tracker.php
@@ -62,6 +62,8 @@ class Gravity_Forms_Page_Tracker {
         
         // Add action link to view posts that contain the form
         add_filter( 'gform_form_actions', array( $this , 'add_form_post_action' ), 10, 2 );
+        
+        add_action( 'init', array( $this, 'process_handler' ) );
       
     }
     
