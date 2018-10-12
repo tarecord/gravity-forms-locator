8f3fd99bc387fac73784b34dbeb53c8b43757969
Tanner Record <tanner.record@gmail.com>
2018-10-12 10:40:22 -0400

    Direct tests to run in the correct directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/gravity-form-locator.php';
+	require dirname( dirname( __FILE__ ) ) . '/src/gf-form-locator/gravity-form-locator.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
