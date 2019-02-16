13da9dba7a83ed18311032a8e9b2337e8f1fc0d6
Tanner Record <tanner.record@gmail.com>
2019-02-16 12:06:14 -0500

    Remove duplicated code

--- src/gf-form-locator/classes/class-wp-scan-existing-forms.php
+++ src/gf-form-locator/classes/class-wp-scan-existing-forms.php
@@ -38,18 +38,11 @@ class WP_Scan_Existing_Forms extends WP_Background_Process {
 
 		$pattern = get_shortcode_regex();
 
-		// Check if shortcode exists in the content.
-		if ( preg_match_all( '/' . $pattern . '/s', $content, $matches ) && array_key_exists( 2, $matches ) && in_array( 'gravityform', $matches[2] ) ) {
-
-			// Use the match to extract the form id from the shortcode.
-			preg_match( '~id="(.*)\"~', $matches[3][0], $form_id );
-
-			// Convert the form id to an integer.
-			$form_id = (int) $form_id[1];
-		}
 		$gravity_form_locator = new Gravity_Form_Locator();
 
-		if ( isset( $form_id ) ) {
+		$form_id = $gravity_form_locator->check_for_form( $content, $pattern );
+
+		if ( $form_id ) {
 
 			$gravity_form_locator->add_form_post_relation( $form_id, $post->ID );
 
