ddfa8c7f684cbf17fae81652c9875b7feab2e2dd
Tanner Record <tanner.record@gmail.com>
2020-03-09 08:22:01 -0400

    Update dependency checking

--- src/gf-form-locator/gravity-form-locator.php
+++ src/gf-form-locator/gravity-form-locator.php
@@ -34,14 +34,53 @@
  * If not, display an error to the user explaining why this plugin
  * could not be activated.
  */
-require_once( 'vendor/class-wps-extend-plugin.php' );
-require_once( 'classes/class-gravity-form-locator.php' );
 
-new WPS_Extend_Plugin( 'gravityforms/gravityforms.php', __FILE__, '2.0', 'gravity-form-locator' );
+add_action( 'plugins_loaded', 'dependency_check' );
 
-// Handle activation and uninstalling.
-register_activation_hook( __FILE__, array( 'Gravity_Form_Locator', 'activate' ) );
-register_uninstall_hook( __FILE__, array( 'Gravity_Form_Locator', 'uninstall' ) );
+/**
+ * Checks to see if Gravity Forms is installed, activated and the correct version.
+ */
+function dependency_check() {
 
-// Now, for the main event.
-$gravity_form_locator = new Gravity_Form_Locator();
+	// If Parent Plugin is NOT active.
+	if ( current_user_can( 'activate_plugins' ) && ! class_exists( 'GFForms' ) ) {
+
+		add_action( 'admin_init', 'my_plugin_deactivate' );
+		add_action( 'admin_notices', 'my_plugin_admin_notice' );
+
+		/**
+		 * Deactivate the plugin.
+		 */
+		function my_plugin_deactivate() {
+			deactivate_plugins( plugin_basename( __FILE__ ) );
+		}
+
+		/**
+		 * Throw an Alert to tell the Admin why it didn't activate.
+		 */
+		function my_plugin_admin_notice() {
+			$gffl_child_plugin  = __( 'Gravity Form Locator', 'gravity-form-locator' );
+			$gffl_parent_plugin = __( 'Gravity Forms', 'gravity-form-locator' );
+
+			echo sprintf(
+				'<div class="error"><p>%1$s requires %2$s to function correctly. Please activate %2$s before activating %1$s. For now, the plugin has been deactivated.</p></div>',
+				'<strong>' . esc_html( $gffl_child_plugin ) . '</strong>',
+				'<strong>' . esc_html( $gffl_parent_plugin ) . '</strong>'
+			);
+
+			if ( isset( $_GET['activate'] ) ) {
+				unset( $_GET['activate'] );
+			}
+		}
+	} else {
+
+		// Start up the plugin.
+		require_once 'classes/class-gravity-form-locator.php';
+
+		// Handle activation and uninstalling.
+		register_activation_hook( __FILE__, array( 'Gravity_Form_Locator', 'activate' ) );
+		register_uninstall_hook( __FILE__, array( 'Gravity_Form_Locator', 'uninstall' ) );
+
+		new Gravity_Form_Locator();
+	}
+}
