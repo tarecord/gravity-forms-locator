13e8e9778d7e4c9c4cb1cbed0fe2dcc2670de52a
Tanner Record <tanner.record@gmail.com>
2020-07-03 15:47:04 -0400

    Get plugin unit tests running

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,21 +5,10 @@
  * @package Gravity_Form_Locator
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
+require '/wp-phpunit/includes/functions.php';
 
-// Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+tests_add_filter( 'muplugins_loaded', function () {
+	require dirname( __FILE__ ) . '/../src/gravity-form-locator.php';
+} );
 
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/src/gf-form-locator/gravity-form-locator.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-// Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require '/wp-phpunit/includes/bootstrap.php';
