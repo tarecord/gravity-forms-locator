c15fa8ca2a66895d97d992207e6894c186a786c6
Tanner Record <tanner.record@gmail.com>
2020-10-16 10:21:42 -0400

    Fix phpcs issues

--- tests/test-gravity-form-locator.php
+++ tests/test-gravity-form-locator.php
@@ -4,7 +4,9 @@
  *
  * @package Gravity_Form_Locator
  */
+
 use PHPUnit\Framework\TestCase;
+use TAR\GravityFormLocator\Core;
 
 /**
  * Responsible for testing Core.
@@ -12,56 +14,52 @@ use PHPUnit\Framework\TestCase;
 class CoreTest extends TestCase {
 
 	/**
-	 * The instantiated class.
+	 * Checks that the function detects all possible shortcode ids.
 	 *
-	 * @var $core
+	 * @param array $shortcodes An array of shortcodes to check.
+	 * @param array $expected   An array of ids found.
+	 * @dataProvider data_get_shortcode_ids
 	 */
-	protected $core;
-
-	public function setUp() {
-		$this->core = new Core();
-    }
-
-	/**
-     * @dataProvider data_get_shortcode_ids
-     */
 	public function test_get_shortcode_ids( $shortcodes, $expected ) {
-		$result = $this->core->get_shortcode_ids( $shortcodes );
+		$result = ( new Core() )->get_shortcode_ids( $shortcodes );
 		$this->assertEquals( $expected, $result );
 	}
 
+	/**
+	 * The dataprovider for checking shortcode ids.
+	 */
 	public function data_get_shortcode_ids() {
 		return array(
-			'standard' => array(
+			'standard'   => array(
 				array(
-					'[gravityform id="10" title="false" description="false"]'
+					'[gravityform id="10" title="false" description="false"]',
 				),
-				array(10)
+				array( 10 ),
 			),
-			'minimal' => array(
+			'minimal'    => array(
 				array(
-					'[gravityform id="1"]'
+					'[gravityform id="1"]',
 				),
-				array(1)
+				array( 1 ),
 			),
-			'huge ID' => array(
+			'huge ID'    => array(
 				array(
-					'[gravityform id="1123456789123456789"]'
+					'[gravityform id="1123456789123456789"]',
 				),
-				array(1123456789123456789)
+				array( 1123456789123456789 ),
 			),
-			'multiple' => array(
+			'multiple'   => array(
 				array(
 					'[gravityform id="1" title="false" description="false"]',
-					'[gravityform id="2" title="false" description="false"]'
+					'[gravityform id="2" title="false" description="false"]',
 				),
-				array(1, 2)
+				array( 1, 2 ),
 			),
 			'missing ID' => array(
 				array(
-					'[gravityform title="false" description="false"]'
+					'[gravityform title="false" description="false"]',
 				),
-				false
+				false,
 			),
 		);
 	}
