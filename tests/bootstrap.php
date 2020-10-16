37653cdb1e352a4a1698e2dd5ea04340470c86dc
Tanner Record <tanner.record@gmail.com>
2020-10-16 10:25:10 -0400

    Reset bootstrap to use default configuration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,28 @@
  * @package Gravity_Form_Locator
  */
 
-require '/wp-phpunit/includes/functions.php';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-tests_add_filter( 'muplugins_loaded', function () {
-	require dirname( __FILE__ ) . '/../src/gravity-form-locator.php';
-} );
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
 
-require '/wp-phpunit/includes/bootstrap.php';
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/gravity-form-locator.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
+
